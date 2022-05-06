a693bb55973f77e45e7c5619392ad3d42d7f3a99
alyt89 <alytrodden1@gmail.com>
2022-05-06 19:32:12 +0000

    add basic game layout and code for random colour array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello!")
+const colours = ["green", "yellow", "blue", "red"];
+
+function pickFourRandomColours() {
+    let i = 0;
+    let coloursList = [];
+    while (i < 4) {
+    coloursList.push(colours[Math.floor(Math.random() * 4)]);
+    i++;
+    }
+    console.log(coloursList);
+    return coloursList;
+}
+let fourRandomColours = pickFourRandomColours();
+
+let redButton = document.getElementById("red-button");
+redButton.addEventListener("click", pickFourRandomColours)
