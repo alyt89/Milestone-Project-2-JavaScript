f01d993d72ca2ac82e427fcce5dea2440e857f56
alyt89 <alytrodden1@gmail.com>
2022-05-07 11:58:41 +0000

    add code for displaying randomcolours contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,14 @@ const yellowButton = document.getElementById("yellow-button");
 const greenButton = document.getElementById("green-button");
 const blueButton = document.getElementById("blue-button");
 const startGame = document.getElementById("start-game");
+const colourDisplayText = document.getElementById("colour-display-text");
+const notStarted = "Colours will appear here. Ready? Click Start.";
 
-function pickFourRandomColours() {
+
+function pickRandomColours() {
     let i = 0;
     let coloursList = [];
-    while (i < 4) {
+    while (i < 6) {
     coloursList.push(colours[Math.floor(Math.random() * 4)]);
     i++;
     }
@@ -16,8 +19,51 @@ function pickFourRandomColours() {
     return coloursList;
 }
 
-startGame.addEventListener("click", startGameClick);
+newColoursList = pickRandomColours();
 
 function startGameClick() {
+    colourDisplayText.innerHTML = "";  
     startGame.style.visibility = "hidden";
+  
+    
 }
+
+function displayColours() {
+
+    if (colourDisplayText.innerHTML === notStarted) {
+        console.log("game not started");
+    }
+
+    else {
+        while (newColoursList.length > 0) {
+            firstColour = newColoursList.shift();
+            colourDisplayText.innerHTML = firstColour;
+            console.log(firstColour);
+            }
+        }
+    
+}
+
+
+
+
+startGame.addEventListener("click", startGameClick);
+startGame.addEventListener("click", displayColours);
+
+// function registerGreenButtonClick();
+
+// function registerBlueButtonClick();
+
+// function registerYellowButtonClick();
+
+// function registerRedButtonClick();
+
+// function checkAnswer();
+
+// function displayResult();
+
+// function increaseScore();
+
+// function nextGame();
+
+
